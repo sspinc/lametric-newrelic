3c8ebf67359644484c92b745a1049c5346803572
Péter <frozenbubble@outlook.com>
2016-12-16 14:10:18 +0100

    new route for api status

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,15 @@ import "fmt"
 
 func HandleRequests() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/apistatus", apiStatus)
 
 	http.ListenAndServe(":5000", nil)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
+}
+
+func apiStatus(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Fit predictor api status")
 }
